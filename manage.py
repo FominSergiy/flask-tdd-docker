82be764846180eb29c75c00fca6a3bef21430abd
Sergiy Fomin <fominssergiy@gmail.com>
2022-01-24 20:23:33 -0500

    adding in a flask cli command to reset the db

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 from flask.cli import FlaskGroup
 
 from src import app
+from src.db import db
 
 cli = FlaskGroup(app)
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__=='__main__':
     cli()
